25809ea6350d3c1492b2ce6b9b3f48b2244d6fe4
harsh <93788928+harsh23079@users.noreply.github.com>
2023-03-14 09:38:50 +0530

    first-commit

--- Module.js
+++ Module.js
@@ -22,13 +22,123 @@
 const path = require("path");
 
 // dirname
-console.log("Folder name:", path.dirname(__filename));
+// console.log("Folder name:", path.dirname(__filename));
 
 // extension name
-console.log("extension name:", path.extname(__filename));
+// console.log("extension name:", path.extname(__filename));
 
 // filename
-console.log("File name:", path.basename(__filename));
+// console.log("File name:", path.basename(__filename));
 
 // parse
-console.log("parse:", path.parse(__filename));
+// console.log("parse:", path.parse(__filename));
+
+// join
+
+// console.log("Join :", path.join(__dirname, "order", "app.js"));
+
+//File system
+const fs = require("fs");
+
+//Make a directory
+// fs.mkdir(path.join(__dirname, "/test"), (err) => {
+//   if (err) {
+//     console.log("Something went wrong...");
+//     console.log(err);
+//     return;
+//   }
+//   console.log("Folder created...");
+// });
+
+// Create a file
+// over write the content of previous data
+
+// fs.writeFile(
+//   path.join(__dirname, "test", "test.txt"),
+//   "Hello Node\n",
+//   (err) => {
+//     if (err) {
+//       throw err;
+//     }
+//     fs.appendFile(
+//       path.join(__dirname, "test3", "test.txt"),
+//       "More Data",
+//       (err) => {
+//         if (err) {
+//           throw err;
+//         }
+//         console.log("Data is added...");
+//       }
+//     );
+//     console.log("File created...");
+//   }
+// );
+
+// Read a File
+
+// fs.readFile(path.join(__dirname, "test", "test.txt"), (err, data) => {
+//   if (err) {
+//     throw err;
+//   }
+//   // const content = Buffer.from(data);
+//   // console.log(content.toString());
+
+//   console.log(data.toString());
+// });
+
+// fs.readFile(path.join(__dirname, "test", "test.txt"), (err, data) => {
+//   if (err) {
+//     throw err;
+//   }
+//   const content = Buffer.from(data);
+// console.log(content.toString());
+
+// console.log(data.toString());
+// });
+
+// const os = require("os");
+// o module
+// console.log("Cpu architecture", os.cpus());
+
+// console.log("total and free memory", os.totalmem(), os.freemem());
+
+// Events module has class so to make object
+
+// const Emitter = require("events");
+
+// const myEmitter = new Emitter();
+
+// myEmitter.on("somename", (data) => {
+//   console.log(data);
+// });
+
+// myEmitter.emit("somename", {
+//   name: "Harsh",
+// });
+
+// custom class using js
+
+// class Auth extends Emitter {
+//   register(username) {
+//     console.log("Registered Sucessfully....");
+//     this.emit("registered", username);
+//   }
+// }
+
+// const auth = new Auth();
+
+// auth.register("harsh mishra");
+
+// listen
+
+// verify email
+// auth.on("registered", (data) => {
+//   console.log(`Sending email to ${data}`);
+// });
+
+// welcome email
+// auth.on("registered", (data) => {
+//   console.log(`Sending welcome email to ${data}`);
+// });
+
+// auth.register("harsh mishra");
--- index.js
+++ index.js
@@ -1,1 +1,82 @@
-console.log("hello mr");
+// console.log("hello mr");
+
+// http module
+
+const http = require("http");
+
+const fs = require("fs");
+const path = require("path");
+
+const app = http.createServer((req, res) => {
+  // console.log(req.url);
+  // for plain text use plain for html
+  res.writeHead(200, { "Content-Type": "text/html" });
+
+  // if (req.url === "/") {
+  //   fs.readFile(
+  //     path.join(__dirname, "public", "index.html"),
+  //     (err, content) => {
+  //       if (err) {
+  //         throw err;
+  //       }
+  //       res.end(content);
+  //     }
+  //   );
+  // } else if (req.url === "/about") {
+  //   fs.readFile(
+  //     path.join(__dirname, "public", "about.html"),
+  //     (err, content) => {
+  //       if (err) {
+  //         throw err;
+  //       }
+  //       res.end(content);
+  //     }
+  //   );
+  // }
+
+  let filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "index.html" : req.url + ".html"
+  );
+
+  fs.readFile(filePath, (err, content) => {
+    //   if (err) {
+    //     res.writeHead(500);
+    //     res.end("Error!!!");
+    //   }
+    // if (err) {
+    // }
+    if (err) {
+      fs.readFile(
+        path.join(__dirname, "public", "error.html"),
+        (err, content) => {
+          if (err) {
+            // internal server error
+            res.writeHead(500);
+            res.end("Error!!!");
+          } else {
+            // page not found
+            res.writeHead(404, {
+              "Content-Type": "text/html",
+            });
+            res.end(content);
+          }
+        }
+      );
+    } else {
+      res.writeHead(200, {
+        "Content-Type": "text/html",
+      });
+      res.end(content);
+    }
+  });
+
+  // res.end("<h1>hello</h1>");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:3000`);
+});
